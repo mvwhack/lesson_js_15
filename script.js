34bacded079b84f23639b9b55481c7a6f0b4e94e
Vlad Mansurov <m_v2709@mail.ru>
2022-06-19 23:12:45 +0300

    Выполнил усложненное задание

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const DomElement = function () {
   this.width = 0;
   this.bg = '';
   this.fontSize = 0;
-
 };
 
 DomElement.prototype.newElem = function () {
@@ -23,17 +22,54 @@ DomElement.prototype.newElem = function () {
     height: ` + this.height + `px;
     width: ` + this.width + `px;
     text-align: center;
-    font - size: ` + this.fontSize + `px;
+    font-size: ` + this.fontSize + `px;
+    position: ` + this.position + `;
   `;
-  div.innerHTML = '<p>Привет мир JavaScript!';
+  div.innerHTML = '<span>Нажми на меня и двигай стрелками</span>';
 };
 
 const blockDiv = new DomElement();
 
-blockDiv.selector = '#block';
+blockDiv.selector = '.block';
 blockDiv.height = 100;
-blockDiv.width = 300;
+blockDiv.width = 100;
 blockDiv.bg = '#000000';
-blockDiv.fontSize = 12;
-
+blockDiv.fontSize = 18;
+blockDiv.position = 'absolute';
 blockDiv.newElem();
+
+
+const elem = document.querySelector('div');
+elem.tabIndex = 0;
+document.body.style.overflow = "hidden";
+
+elem.addEventListener("keydown", function (event) {
+  if (event.code != "ArrowRight" && event.code != "ArrowLeft" &&
+    event.code != "ArrowUp" && event.code != "ArrowDown") {
+    return;
+  }
+
+  let rectElem = elem.getBoundingClientRect();
+
+  console.log(rectElem);
+
+  let x = rectElem.x,
+    y = rectElem.y;
+
+  if (event.code == "ArrowRight") {
+    x += 10;
+  }
+  if (event.code == "ArrowLeft") {
+    x -= 10;
+  }
+  if (event.code == "ArrowUp") {
+    y -= 10;
+  }
+  if (event.code == "ArrowDown") {
+    y += 10;
+  }
+
+  elem.style.position = "absolute";
+  elem.style.left = x + "px";
+  elem.style.top = y + "px";
+});
